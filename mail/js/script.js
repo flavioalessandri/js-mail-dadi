34e6f3e683b54e6717c3d61c1160becda834ea3e
Flavio <flavio.alessandri@gmail.com>
2020-06-23 16:12:51 +0200

    JS mail

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,21 +1,21 @@
 
 var btn = document.getElementById("btn");
-var lista= ["giacomo", "carlo", "maria", "umberto", "admin"]
+var lista= ["giacomo", "carlo", "maria", "umberto", "admin", "gino"];
 console.log(lista);
 
 btn.addEventListener("click", function(){
 
   var mail = document.getElementById("mail").value;
-  console.log(mail);
+  console.log("hai digitato" , mail);
 
-  for ( var i=0; lista.length > i ; i++  ) {
+  for ( var i=0; lista.length > i ; i++) {
+    console.log(lista[i]);
 
-    if(mail === lista[i])  {
-      document.getElementById("risultato").innerHTML = "PUOI ACCEDERE!";
-    }else{
+    if(mail == lista[i]) {
+      document.getElementById("risultato").innerHTML = "PUOI ACCEDERE!" + lista[i];
+    } else{
       document.getElementById("risultato").innerHTML = " NON TI RICONOSCO!";
     }
-
-  };
+  }
 
 });
