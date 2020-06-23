37f8d76327b6bc67607685b0ac7382b85b508948
Flavio <flavio.alessandri@gmail.com>
2020-06-23 16:28:15 +0200

    JS - mail con variabile di appoggio

--- mail/js/script.js
+++ mail/js/script.js
@@ -3,19 +3,19 @@ var btn = document.getElementById("btn");
 var lista= ["giacomo", "carlo", "maria", "umberto", "admin", "gino"];
 console.log(lista);
 
-btn.addEventListener("click", function(){
 
+
+btn.addEventListener("click", function(){
+  var verifica;
   var mail = document.getElementById("mail").value;
   console.log("hai digitato" , mail);
 
   for ( var i=0; lista.length > i ; i++) {
-    console.log(lista[i]);
-
     if(mail == lista[i]) {
-      document.getElementById("risultato").innerHTML = "PUOI ACCEDERE!" + lista[i];
+      var ok = lista[i];
     } else{
       document.getElementById("risultato").innerHTML = " NON TI RICONOSCO!";
     }
   }
-
+  document.getElementById("risultato").innerHTML = verifica;
 });
