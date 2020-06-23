07431d556e917d53b1b7aadc63bd2b01546c0d0c
Flavio <flavio.alessandri@gmail.com>
2020-06-23 17:15:20 +0200

    JS - modifica del nome delle variabili e commenti

--- mail/js/script.js
+++ mail/js/script.js
@@ -9,20 +9,21 @@ console.log(lista);
 //aggiungo un "ascoltatore" al click del mio bottone
 btn.addEventListener("click", function(){
 
-  var verify = false;   //variabile booleana di appoggio
+  var verifyMail = false;   //variabile booleana di appoggio
   var mail = document.getElementById("mail").value;
   console.log("hai digitato" , mail);
 
   for ( var i=0; lista.length > i ; i++) {
-    if(mail == lista[i]) {
-     verify = true;
-    } else{
-      document.getElementById("risultato").innerHTML = " NON TI RICONOSCO!";
+    var myMail = lista[i];
+    if(mail == myMail) {
+    verifyMail = true;
     }
   }
 
   // esco dal for e richiamo la variabile di appoggio
-  if (verify == true){
+  if (verifyMail == true){
   document.getElementById("risultato").innerHTML = mail + " BENVENUTO!";
+  } else{
+    document.getElementById("risultato").innerHTML = " MI DISPIACE, LA TUA MAIL NON E'PRESENTE IN ARCHIVIO!";
   }
-});
+}); //chiudo addEventListener di btn
